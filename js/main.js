58176e8d4eb161ac855de3b29d6aff70b63f2698
Lamiaa Selim <lamiaa.selim@cts-egy.com>
2023-08-26 22:25:16 +0300

    portofolio-IT-text

--- js/main.js
+++ js/main.js
@@ -58,33 +58,6 @@ const navbar = document.getElementById("navbar_top");
     });
 // End Fixed Nav Script
 
-// Start Test Script
-$(document).ready(function () {
-
-    $('.client-single').on('click', function (event) {
-       event.preventDefault();
-
-       var active = $(this).hasClass('active');
-
-       var parent = $(this).parents('.testi-wrap');
-
-       if (!active) {
-           var activeBlock = parent.find('.client-single.active');
-
-           var currentPos = $(this).attr('data-position');
-
-           var newPos = activeBlock.attr('data-position');
-
-           activeBlock.removeClass('active').removeClass(newPos).addClass('inactive').addClass(currentPos);
-           activeBlock.attr('data-position', currentPos);
-
-           $(this).addClass('active').removeClass('inactive').removeClass(currentPos).addClass(newPos);
-           $(this).attr('data-position', newPos);
-
-       }
-   });
-
-}(jQuery));
 
 // Logo Slider
 
